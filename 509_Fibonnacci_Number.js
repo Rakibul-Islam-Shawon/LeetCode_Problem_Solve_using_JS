27920d35693e2a926d5a0b125a6fde95a742c95a
Md Rakibul Islam <shawonnstu@gmail.com>
2024-09-11 10:18:15 +0200

    Fibonnachi Number finding

---
+++ 509_Fibonnacci_Number.js
@@ -0,0 +1,17 @@
+var fib = function(n) {
+    if(n<2){
+        return n;
+    }
+    let f0 = 0;
+    let f1 = 1;
+    let f_next = 0;
+    for(let i = 1;i<n; i++){
+        f_next = f0 + f1;
+        f0 = f1;
+        f1 = f_next;
+    }
+
+    return f_next;
+}
+
+console.log(fib(1));
